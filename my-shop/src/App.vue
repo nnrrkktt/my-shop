48792647ff18b96f47a6843d9f0af1f7aa2972a9
nnrrkktt <martakuzminska97@gmail.com>
2025-06-16 19:18:59 +0200

    kodic

--- my-shop/src/App.vue
+++ my-shop/src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ProductList/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+import { defineComponent } from 'vue'
+export default defineComponent({
+})
 </script>
 
 <style>
--- my-shop/vue.config.js
+++ my-shop/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-shop/'  // Назва вашого репозиторію на GitHub!
-    : '/'
+      ? '/my-shop/'
+      : '/'
 };
