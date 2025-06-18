ed1402106217eccb0981f01e00a84c5b4a35899c
nnrrkktt <martakuzminska97@gmail.com>
2025-06-18 19:48:43 +0200

    lp

---
+++ my-shop/src/components/ReviewsComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h2>Відгуки</h2>
+    </div>
+    <table>
+<thead>
+
+<tr>
+
+<th>Im'n</th>
+
+<th>Рейтинг </th>
+
+<th>Коментар</th>
+
+</tr>
+
+</thead>
+
+<tbody>
+
+<tr v-for= "(review, index) in reviews" :key="index">
+
+<td>{{ review.Name }}</td>
+
+<td>{{ review.Rating }}</td>
+
+<td>{{ review.Comment }}</td>
+
+</tr>
+
+</tbody>
+
+</table>
+</template>
