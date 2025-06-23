5e879de71da3f49ec296b11ce99325ebfa1e04eb
nnrrkktt <martakuzminska97@gmail.com>
2025-06-23 18:51:05 +0200

    eror

--- my-shop/src/App.vue
+++ my-shop/src/App.vue
@@ -4,18 +4,20 @@
       <ProductList />
     </main>
     <FooterComponent />
+    <ReviewsComponent />
   </div>
 </template>
 
 <script>
 import ProductList from './components/ProductList.vue'
 import FooterComponent from './components/FooterComponent.vue'
-
+import ReviewsComponent from "@/components/ReviewsComponent.vue"; 
 export default {
   name: 'App',
   components: {
     ProductList,
-    FooterComponent
+    FooterComponent,
+    ReviewsComponent
   }
 }
 </script>
